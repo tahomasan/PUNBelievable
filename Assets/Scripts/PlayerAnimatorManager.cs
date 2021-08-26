8cb7aa97d7ca3d6061b8147a175ba27251b5399e
RIGBY\Scott <tahomasan@icloud.com>
2021-08-26 13:59:50 -0700

    0 - Introduction 1 - Lobby 2 - Lobby UI 3 - Game Scenes 4 - Game Manager & Levels and part of 5 - Building the Player

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+
+namespace com.satsuma.PUNbelievable
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region Private Fields
+
+
+        [SerializeField]
+        private float directionDampTime = 0.25f;
+
+
+        #endregion
+        
+        #region MonoBehaviour Callbacks
+
+        private Animator animator;
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!animator)
+            {
+                return;
+            }
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v < 0)
+            {
+                v = 0;
+            }
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+
+
+        #endregion
+    }
+}
